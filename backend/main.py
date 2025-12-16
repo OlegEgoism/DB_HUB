b8689e826a2032214618115192b7a28eb26c90ab
oleg <olegpustovalov220@gmail.com>
2025-12-16 14:06:54 +0300

    Пагинация в list_connections для Connection

--- backend/main.py
+++ backend/main.py
@@ -45,7 +45,8 @@ app.add_middleware(
 
 """Подключаем роутеры"""
 app.include_router(agreement.router, prefix="/api/v1")
-app.include_router(users.router, prefix="/api/v1")
 app.include_router(auth.router, prefix="/api/v1")
+
+app.include_router(users.router, prefix="/api/v1")
 app.include_router(db_connection.router, prefix="/api/v1")
 app.include_router(db_metrics.router, prefix="/api/v1")
