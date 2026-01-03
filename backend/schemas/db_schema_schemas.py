38d69f924f92d952d5eaa1ed6ef37446938329d0
oleg <olegpustovalov220@gmail.com>
2026-01-03 23:40:06 +0300

    DB SCHEMAS Получить список функций.

--- backend/schemas/db_schema_schemas.py
+++ backend/schemas/db_schema_schemas.py
@@ -85,5 +85,25 @@ class PaginatedMaterializedViewsResponse(BaseModel):
     has_next: bool
     has_prev: bool
     materialized_views: List[MaterializedViewInfo]
+    model_config = ConfigDict(from_attributes=True)
 
+
+class FunctionInfo(BaseModel):
+    schema_name: str
+    function_name: str
+    description: Optional[str] = None
+    definition: str
+
+
+class PaginatedFunctionsResponse(BaseModel):
+    connection_id: int
+    connection_name: str
+    total_functions: int
+    total_filtered_functions: int
+    page: int
+    size: int
+    pages: int
+    has_next: bool
+    has_prev: bool
+    functions: List[FunctionInfo]
     model_config = ConfigDict(from_attributes=True)
